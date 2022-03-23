5fabdc93a3dda06aa325dae84f28169733669730
Andrew Galvin <andrewggalvin@gmail.com>
2022-03-22 20:45:36 -0400

    Designed React login page based off of Paul's design

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,7 +1,43 @@
+import "./Login.css";
+
 export default function Login(props) {
   return (
-    <div>
-      <h1>Login (Public)</h1>
+    <div className="login">
+      <form>
+        <div className="login-container">
+          <h1>
+            <span>Login</span>
+          </h1>
+          <p>Fill in the below form to login.</p>
+
+          <div className="login-input">
+            <label for="email">
+              <b>Email</b>
+            </label>
+            <input
+              type="text"
+              placeholder="Enter Email"
+              name="email"
+              id="email"
+              required
+            />
+
+            <label for="password">
+              <b>Password</b>
+            </label>
+            <input
+              type="password"
+              placeholder="Enter Password"
+              name="password"
+              id="password"
+              required
+            />
+            <button type="submit" class="loginbtn">
+              Login
+            </button>
+          </div>
+        </div>
+      </form>
     </div>
   );
 }
